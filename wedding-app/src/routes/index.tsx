a1a822758776301b05ec0c4eac44d20c8184fb86
MatsVingerhoets <mats@pau.be>
2022-04-22 20:03:16 +0200

    added routing

---
+++ wedding-app/src/routes/index.tsx
@@ -0,0 +1,14 @@
+import { RouteObject } from "./router";
+// import Layout from "app/components/Layout";
+import Home from "./Home";
+
+const routes: RouteObject[] = [
+  {
+    component: Home,
+    // layout: Layout,
+    hasAuthentication: true,
+    path: "/",
+  },
+];
+
+export default routes;
